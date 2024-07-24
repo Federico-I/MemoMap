2f9d28355968a847cc11634079d975e7aabd0403
Federico-I <threxos.48@gmail.com>
2024-07-24 21:45:49 +0100

    centerViewComp-added-mapPositionUpdated

--- src/Components/Map.jsx
+++ src/Components/Map.jsx
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from "./Map.module.css";
 import { useNavigate, useSearchParams } from 'react-router-dom';
-import { MapContainer } from 'react-leaflet';
+import { MapContainer, useMap } from 'react-leaflet';
 import { Marker, Popup, TileLayer } from 'leaflet';
 import { useCities } from '../context/CitiesContext';
 
@@ -12,28 +12,42 @@ function Map() {
 
   const [mapPosition, setMapPosition] = useState([40, 0]);
 
-  const [searchParams, setSearchParams] = useSearchParams();
+  const [searchParams] = useSearchParams();
 
-  const lat = searchParams.get("lat");
-  const lng = searchParams.get("lng");
+  const mapLat = searchParams.get("lat");
+  const mapLng = searchParams.get("lng");
+
+  useEffect(
+    function() {
+      if(mapLat && mapLng) setMapPosition([mapLat, mapLng]);
+    },
+    [mapLat, mapLng]
+  );
 
   return (
     <div className={styles.mapContainer} onClick={() => navigate("form")}>
-      <MapContainer center={mapPosition} zoom={13} scrollWheelZoom={true} className={styles.map}>
+      <MapContainer center={[mapLat, mapLng]} zoom={8} scrollWheelZoom={true} className={styles.map}>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
         />
-        { cities.map((city) => {
+        { cities.map((city) => (
           <Marker position={[city.position.lat, city.position.lng]} key={city.id}  >
             <Popup>
              <span>{city.emoji}</span> <span>{city.cityName}</span>
             </Popup>
-        </Marker>
-        })}
+          </Marker>
+        ))}
+        <CenterView position={mapPosition} />
       </MapContainer>
     </div>
   )
+};
+
+function CenterView({ position }) {
+  const map = useMap();
+  map.setView(position);
+  return(null);
 };
 
 export default Map;
