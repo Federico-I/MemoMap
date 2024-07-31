a7e82c3389ade9805183e97c5080dfa026b3380c
Federico-I <threxos.48@gmail.com>
2024-07-31 11:54:43 +0100

    FakeContextAuth-setup-reducer-AuthProvider

---
+++ src/context/FakeAuthContext.jsx
@@ -0,0 +1,40 @@
+import { createContext, useContext, useReducer } from 'react';
+
+const AuthContext = createContext();
+
+const initialState = {
+  user: null,
+  isAuthenticated: false,
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "login": 
+      return { ...state, user: action.payload, isAuthenticated: true};
+    case "logout":
+      return { ...state, user: null, isAuthenticated: false};
+    default:
+      throw new Error("Unknown action");
+  };
+}
+
+
+function AuthProvider({ children }) {
+  const [{ user, isAuthenticated, dispatch }] = useReducer( reducer, initialState);
+
+  function logIn( emial, password ) {}
+
+  function logOut() {}
+
+  return (
+    <AuthContext.Provider>{children}</AuthContext.Provider>
+  )
+};
+
+
+function useAuth() {
+  const context = useContext(AuthContext);
+  if(context === undefined)
+    throw new Error("AuthContext was used outside AutgProvider");
+}
+
