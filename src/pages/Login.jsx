723ea732bf2ef2ff74089184f32fcecc768199dd
Federico-I <threxos.48@gmail.com>
2024-08-01 16:00:16 +0100

    implementingAuth-toLogin

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,17 +2,26 @@ import React from "react";
 import { useState } from "react";
 import styles from "./Login.module.css";
 import NavPage from "../Components/NavPage";
+import { useAuth } from "../context/FakeAuthContext";
+import Button from "../Components/Button";
 
 export default function Login() {
   // PRE-FILL FOR DEV PURPOSES
   const [email, setEmail] = useState("jack@example.com");
   const [password, setPassword] = useState("qwerty");
+  const { logIn, logOut } = useAuth();
+
+  function handleSubmit(e){
+    e.preventDefault();
+
+    if (email && password) logIn();
+  }
 
   return (
     <main className={styles.login}>
       <NavPage />
       
-      <form className={styles.form}>
+      <form className={styles.form} onSubmit={handleSubmit}>
         <div className={styles.row}>
           <label htmlFor="email">Email address</label>
           <input
@@ -34,7 +43,7 @@ export default function Login() {
         </div>
 
         <div>
-          <button>Login</button>
+          <Button type="primary">Login</Button>
         </div>
       </form>
     </main>
