df7bc2239f489b09f299b2ae074f4d7183fba4f4
Federico-I <threxos.48@gmail.com>
2024-08-01 17:44:12 +0100

    prefix-login-details-cleared

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -8,8 +8,8 @@ import { useNavigate } from "react-router-dom";
 
 export default function Login() {
   // PRE-FILL FOR DEV PURPOSES
-  const [email, setEmail] = useState("jack@example.com");
-  const [password, setPassword] = useState("qwerty");
+  // const [email, setEmail] = useState("jack@example.com");
+  // const [password, setPassword] = useState("qwerty");
   const { logIn, isAuthenticated } = useAuth();
   const navigate = useNavigate();
 
