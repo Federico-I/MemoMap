2d98a71e3db16ab0a0e16db77c443142e42210f3
Federico-I <threxos.48@gmail.com>
2024-08-02 20:28:52 +0100

    implementingProtectedRoute

---
+++ src/pages/ProtectedRoute.jsx
@@ -0,0 +1,18 @@
+import React, { useEffect } from 'react';
+import { useAuth } from '../context/FakeAuthContext';
+import { useNavigate } from 'react-router-dom';
+
+function ProtectedRoute({ children }) {
+  const {isAuthenticated} = useAuth();
+  const navigate = useNavigate();
+
+  useEffect(function() {
+
+    if (!isAuthenticated) navigate("/");
+
+  }, [isAuthenticated, navigate]);
+
+  return children;
+}
+
+export default ProtectedRoute;
